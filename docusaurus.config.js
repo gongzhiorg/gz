d3df2a6d0debd709f523e878c920dd603de65076
s <hello@gongzhi.org>
2021-03-16 22:50:15 +0800

    add edit this page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,10 +58,8 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-		/*
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-		*/
+            'https://github.com/gongzhiorg/gz/tree/main/',
         },
         blog: {
           showReadingTime: true,
